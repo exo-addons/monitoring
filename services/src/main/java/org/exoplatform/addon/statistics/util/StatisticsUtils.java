855b0933ae22a12eba5cc68da129e5001c45b872
ngammoudi <klmnitts1980>
2016-10-24 15:58:17 +0100

    refactor modules names

---
+++ services/src/main/java/org/exoplatform/addon/statistics/util/StatisticsUtils.java
@@ -0,0 +1,41 @@
+package org.exoplatform.addon.statistics.util;
+
+import org.hibernate.stat.QueryStatistics;
+
+/**
+ * Created ngammoudi exo on 9/16/16.
+ */
+public class StatisticsUtils {
+    public static double toRatio(long leftValue, long rightValue)
+    {
+        if ((leftValue == 0L) && (rightValue == 0L))
+            return 0.0D;
+        return leftValue / (rightValue + leftValue);
+    }
+
+    public static double toTotalAverageTime(QueryStatistics statistics) {
+        return statistics.getExecutionCount() * toAverageExecutionTime(statistics);
+    }
+
+    public static double toAverageExecutionTime(QueryStatistics statistics) {
+        return statistics.getExecutionAvgTime() == 0L ? 0.001D : statistics.getExecutionAvgTime();
+    }
+
+    public static double toQueryPerformance(QueryStatistics statistics) {
+        return toQueryPerformance(toTotalAverageTime(statistics),statistics.getExecutionCount());
+    }
+
+    public static double toQueryPerformance(double totalTimeOnDb, double invocations) {
+        return totalTimeOnDb / invocations;
+    }
+
+    public static double performanceTableCell(double maxPerformance, double performance)
+    {
+
+        return maxPerformance == 0 ? 0 : performance / maxPerformance;
+
+
+
+    }
+
+}
---
+++ webapps/src/main/java/org/exoplatform/addon/statistics/portlet/package-info.java
@@ -0,0 +1,29 @@
+@Portlet
+@Application(name = "Monitoring")
+@Bindings({@Binding(HibernateStatisticsService.class)})
+
+@Scripts({
+       @Script(id = "jquery", value = "js/jquery/jquery-1.12.3.js"),
+        @Script(id = "datatables", value = "js/datatable/jquery.dataTables.js",depends = "jquery"),
+        @Script(id = "datatables-buttons", value = "js/datatable/dataTables.buttons.js",depends={"jquery","datatables"}),
+        @Script(id = "hibernate-statistics-js", value = "js/monitoring/monitoring.js",depends ={"datatables-buttons"})
+})
+
+@Stylesheets({
+
+        @Stylesheet(id = "jquery-datatable-css", value = "css/datatable/jquery.dataTables.css"),
+        @Stylesheet(id = "buttons-datatables-css", value = "css/datatable/buttons.dataTables.css"),
+        @Stylesheet(id = "hibernate-statistics-css", value = "css/monitoring/monitoring.css")})
+@Assets("*")
+package org.exoplatform.addon.statistics.portlet;
+
+import juzu.Application;
+import juzu.plugin.asset.Assets;
+import juzu.plugin.asset.Script;
+import juzu.plugin.asset.Scripts;
+import juzu.plugin.asset.Stylesheet;
+import juzu.plugin.asset.Stylesheets;
+import juzu.plugin.binding.Binding;
+import juzu.plugin.binding.Bindings;
+import juzu.plugin.portlet.Portlet;
+import org.exoplatform.addon.statistics.services.HibernateStatisticsService;
